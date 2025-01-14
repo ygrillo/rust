7e1fd692f4608fd36ac2d8b84f0850ce8a867103
ygrillo <ygrillo@tutanota.com>
2025-01-14 13:58:06 -0300

    feat: Add class usando if_let

---
+++ src/if_let.rs
@@ -0,0 +1,19 @@
+pub fn conteudo_opcional_if_let() {
+    let conteudo_arquivo = ler_arquivo(String::from(""));
+
+    match &conteudo_arquivo {
+        Some(valor) => println!("{}", valor),
+        None => println!("Conteúdo não existe")
+    };
+
+    println!("{:?}", &conteudo_arquivo);
+
+    if let Some(valor) = conteudo_arquivo {
+        println!("Agora tenho certeza de que há o valor {}", valor);
+    }
+}
+
+fn ler_arquivo_if_let(caminho_arquivo: String) -> Option<String> {
+    Some(String::from("Conteudo do arquivo"))
+    // None
+}
--- src/tipos_opcionais.rs
+++ src/tipos_opcionais.rs
@@ -1,11 +1,15 @@
 pub fn conteudo_opcional() {
     let conteudo_arquivo = ler_arquivo(String::from(""));
-    match conteudo_arquivo {
+
+    match &conteudo_arquivo {
         Some(valor) => println!("{}", valor),
         None => println!("Conteúdo não existe")
     };
+
+    println!("{:?}", &conteudo_arquivo)
 }
 
 fn ler_arquivo(caminho_arquivo: String) -> Option<String> {
     Some(String::from("Conteudo do arquivo"))
+    // None
 }
